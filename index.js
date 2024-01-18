ddd4024f624fa5aa4ca56ee0f9a48650d339534f
Saimum-14028 <md.amiralfahim@gmail.com>
2024-01-18 21:12:59 +0600

    user related api completed alhamdulillah

--- index.js
+++ index.js
@@ -26,6 +26,164 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
    // await client.connect();
+   
+   // user related api
+   app.post('/users', async (req, res) => {
+    const user = req.body;
+    console.log(user);
+    
+    const query = {email: user.email};
+    const existingUser = await userCollection.findOne(query);
+    if(existingUser){
+      return res.send({message: 'User Already Exists!', insertedId: null});
+    }
+    
+    const result = await userCollection.insertOne(user);
+    res.send(result);
+  })
+
+  // Get user role
+  app.get('/users/:email', async (req, res) => {
+    const email = req.params.email
+    const result = await userCollection.findOne({ email })
+    res.send(result)
+  })
+
+  app.get('/userCount', async (req, res) => {
+    let queryObj = {};
+
+    const role = req.query.role;
+
+    if(role){
+      queryObj.role = role;
+    }
+
+    console.log(queryObj);
+
+    const cursor = userCollection.find(queryObj);
+    const result = (await cursor.toArray());
+    //console.log('User: ', result);
+    res.send(result);
+  })
+
+  app.get('/parcelCount', async (req, res) => {
+    let queryObj = {};
+
+    const status = req.query.status;
+
+    if(status){
+      queryObj.status = status;
+    }
+
+    console.log(queryObj);
+
+    const cursor = parcelCollection.find(queryObj);
+    const result = (await cursor.toArray());
+    //console.log('User: ', result);
+    res.send(result);
+  })
+
+  app.get('/bookingdate', async (req, res) => {
+
+    const result = await parcelCollection.aggregate([
+      {
+        $match: {
+          booking_date: { $exists: true, $type: 'string' } // Filter documents with "booking_date" as string
+        }
+      },
+      {
+        $group: {
+          _id: { $dateToString: { format: '%Y-%m-%d', date: { $toDate: '$booking_date' } } },
+          count: { $sum: 1 }
+        }
+      },
+      {
+        $sort: { _id: 1 }
+      }
+    ]).toArray();
+
+    console.log(result);
+
+    res.send(result);
+  })
+
+  app.get('/users', async (req, res) => {
+
+    let queryObj = {};
+    let sortObj = {};
+
+    const role = req.query.role;
+    const email = req.query.email;
+
+    if(role){
+      queryObj.role = role;
+    }
+
+    if(email){
+      queryObj.email = email;
+    }
+
+    const sortField = req.query.sortField;
+    const sortOrder = req.query.sortOrder;
+
+    const sortf = req.query.sortf;
+    const sorto = req.query.sorto;
+
+    if(sortField && sortOrder && sortf && sorto ){
+      sortObj[sortField] = sortOrder;
+      sortObj[sortf] = sorto;
+    }
+
+    console.log(sortObj);
+
+    const page = parseInt(req.query.page);
+    const size = parseInt(req.query.size);
+
+    console.log('pagination query', page, size);
+
+    if(size){
+      const result = await userCollection.find(queryObj)
+      .skip(page * size)
+      .limit(size)
+      .toArray();
+      res.send(result);
+    }
+    else{
+      const cursor = userCollection.find(queryObj).sort(sortObj);
+      const result = await cursor.toArray();
+      res.send(result);
+    }
+  })
+
+  app.put("/users/:email", async (req, res) => {
+    const email = req.params.email;
+    const data = req.body;
+
+    const filter = {
+      email: email,
+      };
+    const options = { upsert: true };
+    const updatedData = {
+      $set: {
+        number: data.number,
+        numberofParcelBooked: data.numberofParcelBooked,
+        totalSpent: data.totalSpent,
+        image: data.image,
+        role: data.role,
+        numberOfParcelDelivered: data.numberOfParcelDelivered,
+        totalReview: data.totalReview,
+        numberOfRating: data.numberOfRating,
+        averageRating: data.averageRating
+        },
+      };
+    const result = await userCollection.updateOne(
+      filter,
+      updatedData,
+      options
+      );
+    res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -35,7 +193,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
 
 app.get('/', (req, res) => {
     res.send('Fast server is running')
